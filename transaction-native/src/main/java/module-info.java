93199161fdaf55e4cd4a9e92ed29adc3b50abbf4
lasinicl <lasini.1021@gmail.com>
2020-10-06 13:49:41 +0530

    Migrate module to jdk11

---
+++ transaction-native/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.ballerina.io.transaction {
+    requires io.ballerina.jvm;
+    requires io.ballerina.lang;
+    requires org.slf4j;
+
+    exports org.ballerinalang.io.transaction;
+    exports org.ballerinalang.io.transaction.compiler;
+}
