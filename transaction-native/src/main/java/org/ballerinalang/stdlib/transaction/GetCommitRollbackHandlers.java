4af843ad6b954c343dbe20bbdc0690f88e15ce09
chiran <pcnfernando@gmail.com>
2021-02-17 10:56:22 +0530

    Revert updating build configs

--- transaction-native/src/main/java/org/ballerinalang/stdlib/transaction/GetCommitRollbackHandlers.java
+++ transaction-native/src/main/java/org/ballerinalang/stdlib/transaction/GetCommitRollbackHandlers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -22,7 +22,7 @@ import io.ballerina.runtime.api.values.BArray;
 import io.ballerina.runtime.transactions.TransactionResourceManager;
 
 /**
- * Extern function transaction:getRollbackHandlerList.
+ * Extern function transaction:getCommitRollbackHandlers.
  *
  * @since Swan Lake
  */
